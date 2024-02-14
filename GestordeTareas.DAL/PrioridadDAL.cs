b89ba43c93e8604b8e575eb52de2eb8a71afccf2
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-02-14 04:35:52 -0600

    metodo create

---
+++ GestordeTareas.DAL/PrioridadDAL.cs
@@ -0,0 +1,23 @@
+﻿using GestordeTaras.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestordeTareas.DAL
+{
+    public class PrioridadDAL
+    {
+        public static async Task<int> CreateAsync(Prioridad prioridad)
+        {
+            int result = 0;
+            using (var contextoBD = new ContextoBD())
+            {
+                contextoBD.Add(prioridad);
+                result = await contextoBD.SaveChangesAsync();
+            }
+            return result;
+        }
+    }
+}
