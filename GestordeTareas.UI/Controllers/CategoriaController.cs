a1eefa6351919511ef286dc2655097df9cd012e7
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-02-14 19:24:35 -0600

    Creacion de la VIsta, el generador para mientras

---
+++ GestordeTareas.UI/Controllers/CategoriaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestordeTareas.UI.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: CategoriaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CategoriaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CategoriaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
