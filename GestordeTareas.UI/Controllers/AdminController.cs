2dd3a66d71261d7ac90cc8fa316faed6402eb366
LesterSerrano <lesterserano66@gmail.com>
2024-09-22 22:12:38 -0600

    cree una carpeta Dashboard a la cual movi los accesos en lugar de tenerlos todos en el menu y modifique cosas de diseño en tarea

---
+++ GestordeTareas.UI/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestordeTareas.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View(); // Retorna la vista Dashboard.cshtml
+        }
+    }
+}
